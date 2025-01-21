0d5670365bc01d1599053eef1d8894dfeeaa1bf1
hamma802 <lumma802@gmail.com>
2025-01-21 19:13:57 +0100

    Position des LoginWindows festgelegt

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -1,2 +1,25 @@
-public class LoginWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginWindow extends JFrame {
+    private JPanel LoginWindow;
+    private JButton loginButton;
+
+    public LoginWindow(String title) {
+        super(title);
+        LoginWindow = new JPanel();
+        loginButton = new JButton();
+
+        this.setMinimumSize(new Dimension(700, 900));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(LoginWindow);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.pack();
+
+        loginButton.setMinimumSize(new Dimension(50, 25));
+
+
+
+    }
 }

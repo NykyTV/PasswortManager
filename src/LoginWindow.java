152b6d5b37e409ec14adc6e321449cb1bccab3ed
23ita042 <lumma802@gmail.com>
2025-01-22 14:49:06 +0100

    Schule

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -4,11 +4,14 @@ import java.awt.*;
 public class LoginWindow extends JFrame {
     private JPanel LoginWindow;
     private JButton loginButton;
+    private JTextField benutzerNameEingabe;
+    private JTextField passwortEingabe;
+    private JLabel benutzerText;
+    private JLabel passwortText;
 
     public LoginWindow(String title) {
         super(title);
         LoginWindow = new JPanel();
-        loginButton = new JButton();
 
         this.setMinimumSize(new Dimension(700, 900));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,9 +20,23 @@ public class LoginWindow extends JFrame {
         this.setResizable(false);
         this.pack();
 
-        loginButton.setMinimumSize(new Dimension(50, 25));
+        addComponents();
+    }
+
+    public void addComponents() {
+        benutzerText = new JLabel("Geben Sie Ihren Benutzername ein");
+        passwortText = new JLabel("Geben Sie Ihr Passwort ein");
+        benutzerNameEingabe = new JTextField(20);
+        passwortEingabe = new JTextField(20);
+        loginButton = new JButton("Klick");
 
 
+        //loginButton.addActionListener();
 
+        LoginWindow.add(benutzerText);
+        LoginWindow.add(benutzerNameEingabe);
+        LoginWindow.add(passwortText);
+        LoginWindow.add(passwortEingabe);
+        LoginWindow.add(loginButton);
     }
 }

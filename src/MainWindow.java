6c93cba805ec4141673c323e61f6778590ccd0b5
23ita045 <niklas@rkieslinger.de>
2025-01-22 14:45:40 +0100

    added Passwort eingabe und button zum hinzufügen

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,22 +1,43 @@
 import javax.swing.*;
 
 public class MainWindow extends JFrame{
-    private JPanel mainWindow;
+    private JTextField textfield_pwInput;
+    private JPanel panel1;
+    private JLabel label_pwLabel;
+    private JButton button_add_password;
 
     public MainWindow(String title)
     {
         super(title);
 
-        mainWindow = new JPanel();
+        initComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 800);
+        setLocationRelativeTo(null);
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(mainWindow);
-        this.pack();
+        setVisible(true);
     }
 
     public static void main(String[] args)
     {
-        JFrame frame = new MainWindow("Passwort Manager");
-        frame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            MainWindow frame = new MainWindow("Passwort Manager");
+        });
+    }
+
+    private void initComponents() {
+        JPanel mainWindow = new JPanel();
+
+        label_pwLabel = new JLabel("Passwort:");
+        textfield_pwInput = new JTextField(20);
+        button_add_password = new JButton("+");
+
+        //mainWindow.setLayout(new BoxLayout(mainWindow, BoxLayout.Y_AXIS));
+
+        mainWindow.add(label_pwLabel);
+        mainWindow.add(textfield_pwInput);
+        mainWindow.add(button_add_password);
+
+        setContentPane(mainWindow);
     }
 }

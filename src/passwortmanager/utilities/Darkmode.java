b98ea79d107b69be1e73037322dc6880848e65d3
hamma802 <lumma802@gmail.com>
2025-03-13 21:45:43 +0100

    Darkmode refactor

--- src/passwortmanager/utilities/Darkmode.java
+++ src/passwortmanager/utilities/Darkmode.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.table.JTableHeader;
 import java.awt.*;
+import java.util.List;
 
 public class Darkmode {
     private LoginWindow loginUtility;
@@ -17,95 +18,120 @@ public class Darkmode {
         Color buttonFarbe;
         Color textFarbe;
         Color hintergrundFarbe;
-        Color titlefarbe;
-        boolean setFarbeTextField;
+        Color titelFarbe;
 
-        //Farben werden gesetzt
+        //Hier werden die Farben gesetzt
         if (active) {
             buttonFarbe = Color.LIGHT_GRAY;
             textFarbe = Color.LIGHT_GRAY;
             hintergrundFarbe = Color.DARK_GRAY;
-            titlefarbe = new Color(173, 216, 230);
-            setFarbeTextField = true;
-            if (loginUtility != null) {
-                loginUtility.LoginWindow.setBackground(hintergrundFarbe);
-            }else if (mainUtility != null) {
-                mainUtility.MainPanel.setBackground(hintergrundFarbe);
-            }
+            titelFarbe = new Color(173, 216, 230);
+            List<JPanel> panelList = createPanelListe();
+            setPanelBackground(panelList, hintergrundFarbe);
         }else {
             buttonFarbe = Color.WHITE;
             textFarbe = Color.BLACK;
             hintergrundFarbe = null;
-            titlefarbe = new Color(0, 102, 204);
-            setFarbeTextField = false;
-            if (loginUtility != null) {
-                loginUtility.LoginWindow.setBackground(hintergrundFarbe);
-            }else if (mainUtility != null) {
-                mainUtility.MainPanel.setBackground(hintergrundFarbe);
-            }
+            titelFarbe = new Color(0, 102, 204);
+            List<JPanel> panelList = createPanelListe();
+            setPanelBackground(panelList, hintergrundFarbe);
         }
 
-        //Die Farbe der Komponenten wird geändert
+        //Hier werden die Farben der Komponenten im Mainwindow angepasst.
         if (mainUtility != null) {
             //Buttons
-            mainUtility.darkModeButton.setBackground(buttonFarbe);
-            mainUtility.button_GeneratePW.setBackground(buttonFarbe);
-            mainUtility.button_ADD.setBackground(buttonFarbe);
-            mainUtility.button_Save.setBackground(buttonFarbe);
-            mainUtility.button_logout.setBackground(buttonFarbe);
-            mainUtility.button_logout.setBackground(buttonFarbe);
+            List<JButton> buttonList = List.of(mainUtility.button_logout, mainUtility.darkModeButton, mainUtility.button_GeneratePW, mainUtility.button_ADD, mainUtility.button_Save);
+            setButtonBackground(buttonList, buttonFarbe);
 
             //Labels
-            mainUtility.label_AppName.setForeground(titlefarbe);
-            mainUtility.label_EntryName.setForeground(textFarbe);
-            mainUtility.label_Username.setForeground(textFarbe);
-            mainUtility.label_Password.setForeground(textFarbe);
-            mainUtility.passwordTable.setForeground(textFarbe);
+            mainUtility.label_AppName.setForeground(titelFarbe);
+            List<JLabel> labelList = List.of(mainUtility.label_EntryName, mainUtility.label_Username, mainUtility.label_Password);
+            setLabelForeground(labelList, textFarbe);;
 
             //Panels
-            mainUtility.TopPanel.setBackground(hintergrundFarbe);
-            mainUtility.MidPanel.setBackground(hintergrundFarbe);
-            mainUtility.passwordTable.setBackground(hintergrundFarbe);
-            mainUtility.passwordTable.setSelectionBackground(Color.LIGHT_GRAY);
-//            JTableHeader header = mainUtility.passwordTable.getTableHeader();
-//            header.setBackground(textFarbe);
-//            header.setForeground(buttonFarbe);
+            List<JPanel> panelList = List.of(mainUtility.TopPanel, mainUtility.MidPanel);
+            setPanelBackground(panelList, hintergrundFarbe);
 
             //Textfelder
-            setFarbeTextField(mainUtility.textfield_EntryName, setFarbeTextField);
-            setFarbeTextField(mainUtility.textfield_Username, setFarbeTextField);
-            setFarbeTextField(mainUtility.textfield_Password, setFarbeTextField);
+            List<JTextField> textFieldList = List.of(mainUtility.textfield_EntryName, mainUtility.textfield_Username, mainUtility.textfield_Password);
+            setFarbeTextField(textFieldList);
+
+            //Passworttabelle
+            mainUtility.passwordTable.setForeground(textFarbe);
+            mainUtility.passwordTable.setBackground(hintergrundFarbe);
+            mainUtility.passwordTable.setSelectionBackground(Color.LIGHT_GRAY);
+            JTableHeader header = mainUtility.passwordTable.getTableHeader();
+            header.setBackground(hintergrundFarbe);
+            header.setForeground(textFarbe);
 
 //            mainUtility.MainPanel.setBackground(buttonFarbe);
         }
 
+        //Hier werden die Farben der Komponenten im Loginwindow angepasst.
         if (loginUtility != null) {
             //Buttons
-            loginUtility.loginButton.setBackground(buttonFarbe);
-            loginUtility.registerButton.setBackground(buttonFarbe);
+            List<JButton> buttonList = List.of(loginUtility.loginButton, loginUtility.registerButton);
+            setButtonBackground(buttonList, buttonFarbe);
 
             //Labels
-            loginUtility.benutzerText.setForeground(textFarbe);
-            loginUtility.passwortText.setForeground(textFarbe);
-            loginUtility.label_title.setForeground(titlefarbe);
+            loginUtility.label_title.setForeground(titelFarbe);
+            List<JLabel> labelList = List.of(loginUtility.benutzerText, loginUtility.passwortText);
+            setLabelForeground(labelList, textFarbe);;
 
             //Textfelder
-            setFarbeTextField(loginUtility.benutzerNameEingabe, setFarbeTextField);
-            setFarbeTextField(loginUtility.passwortEingabe, setFarbeTextField);
+            List<JTextField> textFieldList = List.of(loginUtility.benutzerNameEingabe, loginUtility.passwortEingabe);
+            setFarbeTextField(textFieldList);
         }
     }
 
-    private void setFarbeTextField(JTextField textFeld, boolean darkMode) {
+    //Diese Funktion setzt den Hintergrund von JButtons.
+    private void setButtonBackground(List<JButton> buttonList, Color buttonFarbe) {
+        for (int i = 0; i < buttonList.size(); i++) {
+            buttonList.get(i).setBackground(buttonFarbe);
+        }
+    }
+
+    //Diese Funktion setzt den Vordergrund von JLabels.
+    private void setLabelForeground(List<JLabel> labelList, Color textFarbe) {
+        for (int i = 0; i < labelList.size(); i++) {
+            labelList.get(i).setForeground(textFarbe);
+        }
+    }
+
+    //Diese Funktion setzt den Hintergrund von JPanel.
+    private void setPanelBackground(List<JPanel> panelList, Color hintergrundFarbe) {
+        for (int i = 0; i < panelList.size(); i++) {
+            panelList.get(i).setBackground(hintergrundFarbe);
+        }
+    }
+
+    //Diese Funktion erstellt eine Liste von JPanels für die Panel "LoginWindow" und "MainPanel".
+    private List<JPanel> createPanelListe() {
+        List<JPanel> panelList = List.of();
+        if (loginUtility != null) {
+            panelList = List.of(loginUtility.LoginWindow);
+        }else if (mainUtility != null) {
+            panelList = List.of(mainUtility.MainPanel);
+        }
+        return panelList;
+    }
+
+    //Diese Funktion setzt den Vordergrund, Hintergrund und die Border von JTextFields.
+    private void setFarbeTextField(List<JTextField> textFieldList) {
         if (darkMode) {
             Border textFeldBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
-            textFeld.setForeground(Color.DARK_GRAY);
-            textFeld.setBackground(Color.LIGHT_GRAY);
-            textFeld.setBorder(textFeldBorder);
+            for (int i = 0; i < textFieldList.size(); i++) {
+                textFieldList.get(i).setForeground(Color.DARK_GRAY);
+                textFieldList.get(i).setBackground(Color.LIGHT_GRAY);
+                textFieldList.get(i).setBorder(textFeldBorder);
+            }
         }else {
             Border textFeldBorder = BorderFactory.createLineBorder(Color.GRAY);
-            textFeld.setForeground(null);
-            textFeld.setBackground(null);
-            textFeld.setBorder(textFeldBorder);
+            for (int i = 0; i < textFieldList.size(); i++) {
+                textFieldList.get(i).setForeground(null);
+                textFieldList.get(i).setBackground(null);
+                textFieldList.get(i).setBorder(textFeldBorder);
+            }
         }
     }
 

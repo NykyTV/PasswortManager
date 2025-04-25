f55aeead30de468bf1debfafe4e31277223f639b
niklas <niklas@rkieslinger.de>
2025-04-25 19:11:15 +0200

    refactor

--- src/passwortmanager/utilities/AES.java
+++ src/passwortmanager/utilities/AES.java
@@ -4,13 +4,11 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
-import java.util.Base64;
 
 public class AES {
     private static final int KEY_LENGTH = 256;
